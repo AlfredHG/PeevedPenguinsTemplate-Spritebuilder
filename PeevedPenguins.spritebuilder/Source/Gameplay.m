bc509155e53804efd96aa53aa48c03919df4b2c2
Fabio Dono <fabio.d@ulixe.to>
2015-02-04 23:48:08 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -23,6 +23,9 @@
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
     
+    [_physicsNode addChild: _catapultArm];
+    [_physicsNode addChild: _mouseJointNode];
+    
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
     
@@ -44,7 +47,7 @@
         // move the mouseJointNode to the touch position
         _mouseJointNode.position = touchLocation;
         
-        [_physicsNode addChild: _catapultArm];
+        
         // setup a spring joint between the mouseJointNode and the catapultArm
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
     }
